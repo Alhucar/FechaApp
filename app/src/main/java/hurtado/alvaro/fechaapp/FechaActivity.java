91a583e8415e8de9951e651619fe62f0af28ef5c
Alvaro <ahc_frisdal@hotmail.com>
2017-11-02 08:52:29 +0100

    Primer apartat (esquelet del proyecte)

---
+++ app/src/main/java/hurtado/alvaro/fechaapp/FechaActivity.java
@@ -0,0 +1,13 @@
+package hurtado.alvaro.fechaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FechaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fecha);
+    }
+}
